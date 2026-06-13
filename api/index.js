4e3a53661d4a4be366bbf20755bf49a90b22b7d2
rummanstack <rummanstack@gmail.com>
2026-06-14 03:10:18 +0600

    yyyyyyyyyyyyyyyyyyyeee

--- api/index.js
+++ api/index.js
@@ -13,17 +13,26 @@ async function getRuntime() {
 
 
 export default async function handler(req, res) {
-  const { app } = await getRuntime();
+  try {
+    const { app } = await getRuntime();
 
-  const requestUrl = new URL(req.url || '/', 'http://localhost');
-  const apiPath = requestUrl.searchParams.get('path');
+    const requestUrl = new URL(req.url || '/', 'http://localhost');
+    const apiPath = requestUrl.searchParams.get('path');
 
-  if (apiPath !== null) {
-    requestUrl.searchParams.delete('path');
-    const remainingQuery = requestUrl.searchParams.toString();
-    req.url = `/api/${apiPath}${remainingQuery ? `?${remainingQuery}` : ''}`;
-    req.originalUrl = req.url;
+    if (apiPath !== null) {
+      requestUrl.searchParams.delete('path');
+      const remainingQuery = requestUrl.searchParams.toString();
+      req.url = `/api/${apiPath}${remainingQuery ? `?${remainingQuery}` : ''}`;
+      req.originalUrl = req.url;
+    }
+
+    return app(req, res);
+  } catch (error) {
+    console.error('API handler failed:', error);
+    if (!res.headersSent) {
+      res.status(500).json({
+        message: error?.message || 'Internal Server Error',
+      });
+    }
   }
-
-  return app(req, res);
 }

e80e950809980a18210923701d311bf8930b77f5
rummanstack <rummanstack@gmail.com>
2026-06-23 02:16:51 +0600

    dfdf

--- frontend/src/features/landing/components/DemoSection.jsx
+++ frontend/src/features/landing/components/DemoSection.jsx
@@ -20,16 +20,16 @@ export default function DemoSection({ t }) {
           </div>
 
           <div className="flex flex-col gap-3 sm:flex-row lg:flex-col xl:flex-row">
-            <Link to="/login" className="demo-white-btn">
+            <Link to="/login" className="demo-white-btn group">
               {t('landing.tryDemo')}
-              <ArrowRight size={18} />
+              <ArrowRight size={18} className="transition duration-200 group-hover:translate-x-1" />
             </Link>
-            <a href={`tel:${demoPhone}`} className="demo-outline-btn">
-              <Phone size={18} />
+            <a href={`tel:${demoPhone}`} className="demo-outline-btn group">
+              <span className="demo-btn-icon"><Phone size={15} /></span>
               {t('landing.demo.callNow')}
             </a>
-            <a href={whatsappUrl} target="_blank" rel="noreferrer" className="demo-outline-btn">
-              <MessageCircle size={18} />
+            <a href={whatsappUrl} target="_blank" rel="noreferrer" className="demo-whatsapp-btn group">
+              <span className="demo-btn-icon"><MessageCircle size={15} /></span>
               {t('landing.demo.whatsapp')}
             </a>
           </div>

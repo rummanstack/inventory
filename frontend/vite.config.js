bacbceae566bd97021cf8f7d734eac9347b91057
rummanstack <rummanstack@gmail.com>
2026-06-25 18:03:46 +0600

    fdd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  build: {
-    outDir: "../backend/public/dist",
-  },
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3001",
-      "/uploads": "http://localhost:3001",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    build: {
+      outDir: env.VITE_BUILD_PATH || "../backend/public/dist",
     },
-  },
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": env.VITE_API_URL,
+        "/uploads": env.VITE_API_URL,
+      },
+    },
+  };
 });

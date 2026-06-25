eabc9c24a04ed11e5df8121141c7b27c6ba79369
rummanstack <rummanstack@gmail.com>
2026-06-26 01:57:12 +0600

    ss

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  build: {
-    outDir: "../backend/public/dist",
-  },
   plugins: [react()],
   server: {
     proxy: {
